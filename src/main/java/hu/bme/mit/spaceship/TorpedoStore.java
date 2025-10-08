c22569173544c30e1dbf3248e67c37c58c1b3773
unknown <161024@bme.hu>
2025-10-08 15:43:16 +0200

    added comment

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -13,6 +13,7 @@ public class TorpedoStore {
   private double FAILURE_RATE = 0.0; //NOSONAR
 
   private int torpedoCount = 0;
+  // saved and reused the random nmb generator
   private Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
@@ -31,7 +32,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");   
     }
 
     boolean success = false;
